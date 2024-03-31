d3d2744e1d02639eeb424375160e528c6fbd2885
Madhubhashana Mahindarathne <104136158+Madhubhashana99@users.noreply.github.com>
2024-03-31 22:33:04 +0530

    update

--- Notification/src/main/java/com/example/Notification/UserController.java
+++ Notification/src/main/java/com/example/Notification/UserController.java
@@ -1,4 +1,41 @@
 package com.example.Notification;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserRepo userRepository;
+
+    @GetMapping("/")
+    public List<UserEntity> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/")
+    public UserEntity createUser(@RequestBody UserEntity user) {
+        return userRepository.save(user);
+    }
+
+    @GetMapping("/{id}")
+    public UserEntity getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id " + id));
+    }
+
+    @PutMapping("/{id}")
+    public UserEntity updateUser(@PathVariable Long id, @RequestBody UserEntity user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
 }
--- Notification/src/main/java/com/example/Notification/UserEntity.java
+++ Notification/src/main/java/com/example/Notification/UserEntity.java
@@ -11,4 +11,36 @@ public class UserEntity {
     private String username;
     private String address;
     private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
